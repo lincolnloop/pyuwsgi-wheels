155e3372eeee3259619859e91b1feb97f9529a4b
Peter Baumgartner <pete@lincolnloop.com>
2018-09-10 16:27:04 -0600

    Include libjansson

--- config.sh
+++ config.sh
@@ -1,9 +1,28 @@
+#!/bin/bash
+
+JANSSON_HASH=6e85f42dabe49a7831dbdd6d30dca8a966956b51a9a50ed534b82afc3fa5b2f4
+JANSSON_DOWNLOAD_URL=http://www.digip.org/jansson/releases
+JANSSON_ROOT=jansson-2.11
+
+
 function pre_build {
     mv uwsgi/setup.pyuwsgi.py uwsgi/setup.py
     sed 's/uwsgiconfig\.uwsgi_version/uwsgiconfig.uwsgi_version + "'$APPEND_VERSION'"/' uwsgi/setup.pyuwsgi.py
     build_pcre
     build_libyaml
     build_zlib
+    build_jansson
+}
+
+function build_jansson {
+    if [ -e jansson-stamp ]; then return; fi
+    fetch_unpack ${JANSSON_DOWNLOAD_URL}/${JANSSON_ROOT}.tar.gz
+    check_sha256sum $ARCHIVE_SDIR/${JANSSON_ROOT}.tar.gz ${JANSSON_HASH}
+    (cd ${JANSSON_ROOT} \
+        && ./configure --prefix=$BUILD_PREFIX \
+        && make -j4 \
+        && make install)
+    touch jansson-stamp
 }
 
 function run_tests {
