cd9ac0ceb04f62166010819c9dd22e47d2815ef2
Peter Baumgartner <pete@lincolnloop.com>
2018-09-10 14:28:06 -0600

    Add option for development versions

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 function pre_build {
     mv uwsgi/setup.pyuwsgi.py uwsgi/setup.py
+    sed 's/uwsgiconfig\.uwsgi_version/uwsgiconfig.uwsgi_version + "'$APPEND_VERSION'"/' uwsgi/setup.pyuwsgi.py
     build_pcre
     build_libyaml
     build_zlib
