dc7d9473778939f2ef5ce4c739450a90422bc5f9
Peter Baumgartner <pete@lincolnloop.com>
2018-09-14 20:50:27 -0600

    Fix mac builds

--- patch-uwsgi-packaging.sh
+++ patch-uwsgi-packaging.sh
@@ -8,4 +8,6 @@ APPEND_VERSION=${APPEND_VERSION:-""}
 mv $SRC/setup.pyuwsgi.py $SRC/setup.py
 sed -i.bak 's/uwsgiconfig\.uwsgi_version/uwsgiconfig.uwsgi_version + "'$APPEND_VERSION'"/' $SRC/setup.py
 rm $SRC/setup.py.bak
+cp $SRC/buildconf/pyuwsgi.ini $SRC/buildconf/pyuwsginossl.ini
+echo "ssl = false" >> $SRC/buildconf/pyuwsginossl.ini
 rm $SRC/PKG-INFO
