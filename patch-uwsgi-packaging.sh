54b09d62ccfd7de459b280340d747c4f44906fd8
Peter Baumgartner <pete@lincolnloop.com>
2019-02-14 11:56:53 -0700

    Create a `uwsgi` script in addition to `pyuwsgi`
    Makes pyuwsgi a drop-in replacement for uwsgi, similar to
    psycopg2-binary and psycopg2. Discussion here: https://github.com/unbit/uwsgi/pull/1864#discussion_r256900741
    
    Using my own copy of the upstream `setup.pyuwsgi.py` for easier
    management of PyPI details. I don't have commit access to unbit/uwsgi.

--- patch-uwsgi-packaging.sh
+++ patch-uwsgi-packaging.sh
@@ -5,7 +5,7 @@ set -euf -o pipefail
 SRC=$1
 APPEND_VERSION=${APPEND_VERSION:-""}
 
-mv $SRC/setup.pyuwsgi.py $SRC/setup.py
+cp setup.py $SRC/setup.py
 sed -i.bak 's/uwsgiconfig\.uwsgi_version/uwsgiconfig.uwsgi_version + "'$APPEND_VERSION'"/' $SRC/setup.py
 rm $SRC/setup.py.bak
 cp $SRC/buildconf/pyuwsgi.ini $SRC/buildconf/pyuwsginossl.ini
