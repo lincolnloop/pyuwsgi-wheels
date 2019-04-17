f4086dc07549d9bf1bf461b912d99b71a51979ba
Peter Baumgartner <pete@lincolnloop.com>
2019-04-17 09:31:28 -0600

    Test building manylinux2010 wheel

--- config.sh
+++ config.sh
@@ -4,7 +4,6 @@ JANSSON_HASH=6e85f42dabe49a7831dbdd6d30dca8a966956b51a9a50ed534b82afc3fa5b2f4
 JANSSON_DOWNLOAD_URL=http://www.digip.org/jansson/releases
 JANSSON_ROOT=jansson-2.11
 
-
 function pre_build {
     build_pcre
     build_zlib
