1dbd920569cd7137e5f12f64bcfb38a779be6826
Anthony Sottile <asottile@umich.edu>
2024-08-22 12:16:31 -0400

    remove patching of pyuwsginossl.ini
    upstream already provides a working configuration

--- patch-uwsgi-packaging.sh
+++ patch-uwsgi-packaging.sh
@@ -8,6 +8,4 @@ APPEND_VERSION=${APPEND_VERSION:-""}
 cp setup.py $SRC/setup.py
 sed -i.bak 's/uwsgiconfig\.uwsgi_version/uwsgiconfig.uwsgi_version + "'$APPEND_VERSION'"/' $SRC/setup.py
 rm $SRC/setup.py.bak
-cp $SRC/buildconf/pyuwsgi.ini $SRC/buildconf/pyuwsginossl.ini
-echo "ssl = false" >> $SRC/buildconf/pyuwsginossl.ini
 rm $SRC/PKG-INFO
